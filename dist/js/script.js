668a016bcb78e8a4e6e27e23bdf8f579ab794545
Andrei <andreii.rubik@mail.ru>
2023-03-09 18:58:34 +0300

    Commit

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,2 +1,9 @@
 AOS.init();
 
+const hamburg = document.querySelector('.hamburger__lines'),
+      items = document.querySelector('.hamburger__items')
+
+hamburg.addEventListener('click', () => {
+    hamburg.classList.toggle('hamburger__active')
+    items.classList.toggle('hamburger__items-active')
+})
